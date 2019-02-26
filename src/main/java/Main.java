2acd73bba75e1d17d6677296605e5b90a4b5b7e3
yegorf <yegorkhohlov@gmail.com>
2019-02-26 18:43:19 +0300

    start

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.nio.file.Files;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
